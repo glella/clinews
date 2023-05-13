c57c21a9aab2d5ada196651c550501ed48f7c427
Guillermo Lella <arkorott@gmail.com>
2023-05-13 17:45:41 -0400

    refactor & async

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,54 @@
-use colour::{dark_green, yellow};
+mod theme;
+
+// use colour::{dark_green, yellow};
 use dotenv::dotenv;
-use newsapi::{get_articles, Articles};
+use newsapi::{Article, Country, Endpoint, NewsAPI};
 use std::{env, error::Error};
 
-pub fn render_articles(articles: &Articles) {
-    for item in &articles.articles {
-        dark_green!("> {}\n", item.title);
-        yellow!("- {}\n\n", item.url);
+// pub fn render_articles(articles: &Articles) {
+//     for item in &articles.articles {
+//         dark_green!("> {}\n", item.title);
+//         yellow!("- {}\n\n", item.url);
+//     }
+// }
+
+fn render_articles(articles: &Vec<Article>) {
+    let theme = theme::default();
+    theme.print_text("# Top headlines\n\n");
+    for i in articles {
+        theme.print_text(&format!("`{}`", i.title()));
+        theme.print_text(&format!("> *{}*", i.url()));
+        theme.print_text("---");
     }
 }
-fn main() -> Result<(), Box<dyn Error>> {
+
+// fn main() -> Result<(), Box<dyn Error>> {
+//     dotenv().ok(); // load the enviroment variables
+//     let news_api_key = env::var("NEWS_API_KEY").expect("NEWS_API_KEY not set");
+
+//     let mut news_api = NewsAPI::new(&news_api_key);
+//     news_api
+//         .endpoint(Endpoint::TopHeadlines)
+//         .country(Country::Us);
+//     let response = news_api.fetch()?;
+//     let articles = response.articles();
+//     render_articles(articles);
+
+//     Ok(())
+// }
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok(); // load the enviroment variables
     let news_api_key = env::var("NEWS_API_KEY").expect("NEWS_API_KEY not set");
-    let mut url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=".to_owned();
-    url.push_str(&news_api_key);
 
-    let articles = get_articles(&url)?;
-    render_articles(&articles);
+    let mut news_api = NewsAPI::new(&news_api_key);
+    news_api
+        .endpoint(Endpoint::TopHeadlines)
+        .country(Country::Us);
+    let response = news_api.fetch_async().await?;
+    let articles = response.articles();
+    render_articles(articles);
 
     Ok(())
 }
--- src/theme.rs
+++ src/theme.rs
@@ -1,4 +1,32 @@
-use crossterm::style::Color::{AnsiValue, Green, Rgb, Yellow};
-use terminad::{rgb, MadSkin, StyledChar};
+use termimad::crossterm::style::Color::{Rgb, Yellow};
+use termimad::{MadSkin, StyledChar};
 
-pub fn default() {}
+pub fn default() -> MadSkin {
+    let mut skin = MadSkin::default();
+    skin.bold.set_fg(Yellow);
+    skin.italic.set_bg(Rgb {
+        r: 28,
+        g: 28,
+        b: 28,
+    });
+    skin.bullet = StyledChar::from_fg_char(Yellow, '⟡');
+    skin.set_headers_fg(Yellow);
+    skin.quote_mark = StyledChar::from_fg_char(Yellow, '▐');
+    skin.quote_mark.set_fg(Rgb {
+        r: 215,
+        g: 255,
+        b: 135,
+    });
+    skin.inline_code.set_fg(Rgb {
+        r: 66,
+        g: 135,
+        b: 245,
+    });
+    skin.italic.set_fg(Rgb {
+        r: 215,
+        g: 255,
+        b: 0,
+    });
+
+    skin
+}
